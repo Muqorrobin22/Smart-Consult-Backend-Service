4dff6c936b6d01c65eb3013a2e9b60c704d850e5
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-06-06 23:24:42 +0700

    protecting api route for get users when get without authenticated

--- src/users/entities/admin.entity.ts
+++ src/users/entities/admin.entity.ts
@@ -1,10 +1,16 @@
 import { admin_temp } from '@prisma/client';
+import { Exclude } from 'class-transformer';
 
 export class AdminEntity implements admin_temp {
+  constructor(partial: Partial<AdminEntity>) {
+    Object.assign(this, partial);
+  }
+
   id: number;
 
   username: string;
 
+  @Exclude()
   password: string;
 
   email: string;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AdminEntity } from './entities/admin.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id);
+  @UseGuards(JwtAuthGuard)
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return new AdminEntity(await this.usersService.findOne(id));
   }
 }
