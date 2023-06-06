043b45cfde344642adae4b030398775a3642ab65
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-06-06 22:23:58 +0700

    Implement login functionality with JWT success

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,14 +7,23 @@ import {
   Post,
 } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { AuthEntity } from './entity/auth.entity';
+import { LoginDto } from 'src/dto/login/login.dto';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  async findOne(@Body() singInDto) {
-    return this.userService.findOne(singInDto.username);
+  @ApiOkResponse({ type: AuthEntity })
+  login(@Body() { email, password }: LoginDto) {
+    return this.authService.login(email, password);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,56 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthEntity } from './entity/auth.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-  async validateUser(username: string, password: string) {
-    const findUser = await this.userService.findOne(username);
+  async login(email: string, password: string): Promise<AuthEntity> {
+    // Fetch User with a given email
+    const user = await this.prisma.admin_temp.findUnique({
+      where: { email: email },
+    });
 
-    if (findUser && findUser.password === password) {
-      return findUser;
+    //  If No user Found
+    if (!user) {
+      throw new NotFoundException(`No user found for email: ${email}`);
     }
 
-    throw new UnauthorizedException();
+    // Check if the password is correct
+    const isPasswordValid = user.password === password;
+
+    // if password not match
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    // Generate JWT containing user id
+    return {
+      accessToken: this.jwtService.sign({
+        userId: user.id,
+      }),
+    };
   }
+
+  //   Not using this.
+  //   async validateUser(username: string, password: string) {
+  //     const findUser = await this.userService.findOne(username);
+
+  //     if (findUser && findUser.password === password) {
+  //       return findUser;
+  //     }
+
+  //     throw new UnauthorizedException();
+  //   }
 }
