5c0a2996696dd436209c172b45ea3a0a70623b65
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-06-06 17:57:53 +0700

    setup the swagger ui module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  providers: [AuthService, PrismaService],
+  imports: [UsersModule],
+  providers: [AuthService, PrismaService, UsersService],
 })
 export class AuthModule {}
