106dba82dab8ac06701b224e66b965df8f8e94f3
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-06-06 10:48:47 +0700

    create module and service auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiseaseModule } from './disease/disease.module';
 import { BotModule } from './bot/bot.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DiseaseModule, BotModule],
+  imports: [DiseaseModule, BotModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
