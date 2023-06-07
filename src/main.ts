f1f91378b10050be6b893271714e63729baa2a41
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-06-07 15:57:27 +0700

    add cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // app.useGlobalPipes(new ValidationPipe({whitelist: true}))
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
